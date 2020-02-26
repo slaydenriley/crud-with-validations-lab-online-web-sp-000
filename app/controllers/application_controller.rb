e666c8f7be874dc78cac67deee4aafd1c58faad9
Riley Slayden <slaydenriley@gmail.com>
2020-02-26 21:54:58 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,3 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 end
-
-
-rails g resource Song title:string released:boolean release_year:integer artist_name:string genre:string --no-test-framework
