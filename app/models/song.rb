93907caa93d42a6c6fc8053168db4ac101d177ad
Riley Slayden <slaydenriley@gmail.com>
2020-02-26 21:57:59 +0000

    Automatically backed up by Learn

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,4 +1,7 @@
 class Song < ApplicationRecord
   validates :title, presence: true
-
+  validates :title, uniqueness: {
+    scope: %i[release_year artist_name],
+    message: 'cannot be repeated by the same artist in the same year'
+  }
 end
