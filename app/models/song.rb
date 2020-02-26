efbc723b96935c277843ded6cad14e5a3138f594
Riley Slayden <slaydenriley@gmail.com>
2020-02-26 21:59:03 +0000

    Automatically backed up by Learn

--- app/models/song.rb
+++ app/models/song.rb
@@ -6,7 +6,7 @@ class Song < ApplicationRecord
   }
   validates :released, inclusion: { in: [true, false] }
   validates :artist_name, presence: true
-  
+
   with_options if: :released? do |song|
     song.validates :release_year, presence: true
     song.validates :release_year, numericality: {
